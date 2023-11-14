1019e690bc2e44f9ddfed54de7b22c535bea68e9
Alex <ailich@usf.edu>
2023-11-14 11:23:53 -0500

    get_payout patch - In 2023 they changed the row data started on

--- R/get_payout.R
+++ R/get_payout.R
@@ -5,17 +5,24 @@
 #' @param trim_rows A logical that if TRUE (default), will take the median of all values with the same timestamp so that each timestamp has one value
 #' @import dplyr
 #' @importFrom lubridate mdy_hms
+#' @importFrom lubridate POSIXct
 #' @importFrom readr read_csv
 #' @importFrom stats median
 #' @export
 
 get_payout<- function(winch_dir, trim_rows=TRUE){
   file_list<- list.files(path = winch_dir, pattern = "\\.csv$", full.names = TRUE) #List files
-  winch_acc<- tibble(timestamp = NA, Payout_m =NA)[0,] #Create Accumulator Variable
+  winch_acc<- tibble(timestamp = lubridate::POSIXct(), Payout_m =numeric()) #Create Accumulator Variable
   for (i in 1:length(file_list)) {
-    winch_df<- suppressMessages(read_csv(file_list[i], skip = 11, col_names = FALSE)) #Read in relevant rows of data
+    yr<- as.numeric(stringr::str_extract(basename(file_list[i]), "\\d{4}")) #year
+    if (yr < 2023) {
+      skip<- 11
+    } else{
+      skip<-10
+      }
+    winch_df<- suppressMessages(read_csv(file_list[i], skip = skip, col_names = FALSE)) #Read in relevant rows of data
     if (nrow(winch_df) > 0) {
-      winch_names<- read.csv(file_list[i], skip = 7, header = FALSE,nrows=3, colClasses = "character")
+      winch_names<- read.csv(file_list[i], skip = skip-4, header = FALSE,nrows=3, colClasses = "character")
       if(!(winch_names$V2[3]=="Clock" & grepl(pattern = "Block", x = winch_names$V14[1], ignore.case = TRUE) & winch_names$V14[3]=="Payout"))
         {stop("Winch data not formatted as expected")} #Ensure winch data formatted as expected
       winch_df<- winch_df[,c(2,14)] #Select Relevant Columns
