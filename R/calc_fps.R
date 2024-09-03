d30b5298a3ba4720af4a33a07a9319516e4d6244
Alex <ailich@usf.edu>
2024-09-03 10:57:28 -0400

    Add missing helper function - calc_fps needed helper function to detect camera pattern

--- R/calc_fps.R
+++ R/calc_fps.R
@@ -1,3 +1,21 @@
+#' Function for detecting string type in camera table
+#'
+#' Function for detecting string type in camera table
+#' @param cam_filepath vector of filepaths from blackfly camera
+
+detect_cam_pattern<- function(cam_filepath){
+  patterns<- unique(str_replace_all(string = basename(cam_filepath), pattern = "\\d+", replacement = "\\\\d+"))
+  patterns
+  my_pattern<- patterns[patterns != "\\d+-\\d+" & (!grepl(pattern = "*\\.txt", x = patterns))]
+  my_pattern
+  if(length(my_pattern)>1){
+    message("Error: multiple patterns available")
+    stop()}
+  if(length(my_pattern)<1){
+    message("Error: no patterns available")
+    stop()}
+  return(my_pattern)}
+
 #' Calculates frame-rate of Blackfly Camera
 #'
 #' Calculates frame-rate of Blackfly Camera.
